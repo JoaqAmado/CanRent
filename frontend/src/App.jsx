032ef95f1ec1fc1c8981e1ae9bb4d8d93007a8ae
JoaqAmado <wacky.macoolit@gmail.com>
2026-03-06 22:43:55 -0500

    Updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import LandingPage from "./pages/LandingPage/LandingPage.jsx";
+import Property from "./pages/Property/Property.jsx";
+import Search from "./pages/Search/Search.jsx";
+import ErrorPage from "./pages/ErrorPage/ErrorPage.jsx";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <nav>
+        <Link></Link>
+      </nav>
+
+
+      <Routes>
+        <Route path="/" element={<LandingPage/>}></Route>
+        <Route path="/search" element={<Search/>}></Route>
+        <Route path="/property" element={<Property/>}></Route>
+        <Route path="*" element={<ErrorPage/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
