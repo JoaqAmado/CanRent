9d2eef7b76b7c6a1315354b5043e2c1b7813954c
dorianle17-oss <dorian.le.17@hotmail.com>
2026-03-07 11:00:44 -0500

    Added a basic navigation bar.

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -2,8 +2,26 @@ import "./NavBar.css";
 
 function NavBar() {
     return (
-        <>
-        </>
+        <body>
+            <div class="header">
+                {/* logo and welcome message at the top of the landing page. */}
+                <img style="width: 30px;" src="assets/CanRent Logo.png"></img>
+                <span class="welcome-message">Welcome to CanRent</span>
+
+                {/* this section contains buttons linked to the other pages. */}
+                <div class="tabs">
+                    <button class="tabs-button">
+                        <div class="tabs-label">Stuff</div>
+                    </button>
+                    <button class="tabs-button">
+                        <div class="tabs-label">Browse</div>
+                    </button>
+                    <button class="tabs-button">
+                        <div class="tabs-label">Saved</div>
+                    </button>
+                </div>
+            </div>
+        </body>
     );
 }
 export default NavBar;
