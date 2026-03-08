2c7e58d101692e6967887ba9f578ec9269652f19
JoaqAmado <wacky.macoolit@gmail.com>
2026-03-07 21:51:13 -0500

    Completed filtering backend feature

--- backend/CanRent/properties/models.py
+++ backend/CanRent/properties/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Property(models.model):
+    title = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.IntegerField()
+    review = models.DecimalField(max_digits=2, decimal_places=1)
+    imageUrl = models.URLField(max_length=500, blank=True, null=True)
+    bedrooms = models.IntegerField()
+    bathrooms = models.IntegerField()
+    features = models.JSONField(default=list)
+    original_link = models.URLField(max_length=500, blank=True, null=True)
--- backend/CanRent/properties/views.py
+++ backend/CanRent/properties/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Property
+from .serializer import PropertySerializer
 
-# Create your views here.
+
+@api_view(["GET"])
+def properties(request):
+    # Start will all properties
+    postings = Property.objects.all()
+
+    # Gets all values of the request
+    min_price = request.query_params.get("min_price")
+    min_rating = request.query_params.get("min_rating")
+    location = request.query_params.get("location")
+
+    # Only applies if data is provided
+    if min_price:
+        postings = postings.filter(price__gte=min_price)
+    if min_rating:
+        postings = postings.filter(review__gte=min_rating)
+    if location:
+        postings = postings.filter(location__icontains=location)
+
+    serializer = PropertySerializer(postings, many=True)
+    return Response(serializer.data)
